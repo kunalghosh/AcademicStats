d811d8d37d123b639e42e8174bfe0f923a4f15cd
Kunal Ghosh <kunal.ghosh@aalto.fi>
2022-05-01 11:58:59 +0300

    Processing Pubmed and Arxiv and printing countries ordered by num authors

--- academic_stats/__main__.py
+++ academic_stats/__main__.py
@@ -1,7 +1,8 @@
 """Console script for academic_stats."""
+from collections import defaultdict
 import sys
 import click
-from academic_stats.parsers.parser import PubMed
+from academic_stats.parsers.parser import PubMed, Arxiv
 
 
 @click.command()
@@ -21,10 +22,32 @@ from academic_stats.parsers.parser import PubMed
 )
 def main(pubmed, arxiv):
     """Console script for academic_stats."""
+    country_authors_dict = defaultdict(lambda: set([]))
+
     pubmed = PubMed(pubmed)
     for author in pubmed:
         if author.countries != set([]):
-            print(author)
+            if author.name != "":
+                for country in author.countries:
+                    country_authors_dict[country].add(author.name)
+
+    arxiv = Arxiv(arxiv)
+    for author in arxiv:
+        if author.countries != set([]):
+            if author.name != "":
+                for country in author.countries:
+                    country_authors_dict[country].add(author.name)
+
+    # compute number of authors in each country
+    country_nauthor = dict(
+        [(country, len(authors)) for country, authors in country_authors_dict.items()]
+    )
+    # sort by number of authors
+    country_nauthor = sorted(country_nauthor.items(), key=lambda kv: kv[1])
+
+    # sort by number of authors
+    for country, nauthor in reversed(country_nauthor):
+        print(f"{country:28}: {nauthor:5}")
 
 
 if __name__ == "__main__":
